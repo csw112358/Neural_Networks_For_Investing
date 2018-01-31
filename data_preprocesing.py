23eb5c040e059163e49b7399f345d5fea930776f
csw112358 <csw112358@gmail.com>
2018-01-30 18:02:29 -0600

    coded the file read in scheme and set the df index

---
+++ data_preprocesing.py
@@ -0,0 +1,19 @@
+"""This file takes in a .csv file containing company fundamental data and derives the data frame containing the model inputs for a RNN"""
+
+import numpy as np 
+import pandas as pd 
+from matplotlib import pyplot as plt 
+
+# read the company data into a pandas dataframe: df1
+
+filename = 's_and_p_A_ABBV_data.csv'
+df1 = pd.read_csv(filename)
+
+# format the date column to datetime object
+time_format = '%Y%m%d'
+df1['datadate'] = pd.to_datetime(df1['datadate'], format=time_format)
+df1.rename(columns={'datadate': 'date'}, inplace=True)
+
+# reindex to a hierarchical index with date and ticker symbol (tic)
+df1.set_index(['tic', 'date'], inplace=True)
+df1.head()
